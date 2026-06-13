0e2c2e481c1be30bfd2ed1cc4c7c2feecc554438
idwip11 <77789830+idwip11@users.noreply.github.com>
2026-06-13 18:40:37 +0800

    chore: update techStack for NTB demographics to Tableau, MS Excel, QGIS

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -114,7 +114,7 @@ export const SHOWCASES: ShowcaseItem[] = [
       "Dynamic population pyramids comparing age groups and genders",
       "Actionable data insights for government policy and market analysis"
     ],
-    techStack: ["Tableau", "Data Analysis"],
+    techStack: ["Tableau", "MS Excel", "QGIS"],
     tableauEmbed: `<div class='tableauPlaceholder' id='viz1781345293144' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RG&#47;RG58NG6PG&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='path' value='shared&#47;RG58NG6PG' /> <param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RG&#47;RG58NG6PG&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>`
   }
 ];
